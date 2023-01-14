394a3482cabad9dc1700236c20c5b4d6c77e2654
math-pixel <59537610+math-pixel@users.noreply.github.com>
2023-01-14 16:23:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ let button = [...ButtonElm];
 
 const contenu3D = [{titre:"Sabre CyberPunk", image: "https://math-pixel.github.io/portfolio/image/sbre%20cyberpunk.png", link:""},{titre:"Usine", image: "https://math-pixel.github.io/portfolio/image/usine%20rendu.png", link:""},{titre:"Sabre Deamon Slayer", image: "https://math-pixel.github.io/portfolio/image/rengoku%20sword1.png", link:""}, {titre:"Maison Japonaise", image: "https://math-pixel.github.io/portfolio/image/japon%20blender3.png", link:""}];
 
-const contenuPhoto = [{titre:"Drone", image: "./images/drone1.JPG", link:""},{titre:"Photoshop", image: "./images/constellation.png", link:""},{titre:"IA", image: "./images/ia1.png", link:""},{titre:"IA", image: "./images/ia2.png", link:""},{titre:"Photographie", image: "./images/moto.jpg", link:""},{titre:"Photographie", image: "./images/dos.jpg", link:""},{titre:"Photographie", image: "./images/voiture.JPG", link:""}];
+const contenuPhoto = [{titre:"Drone", image: "./images/drone1.JPG", link:""},{titre:"Photographie", image: "./images/voiture.JPG", link:""},{titre:"Drone", image: "./images/drone2.jpg", link:""},{titre:"Drone", image: "./images/drone3.jpg", link:""},{titre:"Photographie", image: "./images/moto.jpg", link:""},{titre:"Photographie", image: "./images/dos.JPG", link:""}];
 
 const contenuCreation = [{titre:"3D Printer", image: "./images/3dprinter2.jpg", link:""},{titre:"Volet Automatique", image: "./images/volet.png", link:""},{titre:"Table de Mixage", image: "https://math-pixel.github.io/portfolio/image/tableMix.jpg", link:""},{titre:"Borne Arcade", image: "https://retropie.org.uk/wp-content/uploads/2016/04/Retropie_Splash.png", link:""}]
 
@@ -170,7 +170,7 @@ setCarrousel(0);
 let contentVoirPlus = [
     {
         name:"Workshop",
-        description:"Un premier Project effectuer en ecole qui consistait a travailler avec des graphistes. <br>L' Objectif etant de creer deux sites vitrines pour un festival avec la contrainte d' avoir une seule page HTML<br>Les deux sites devais avoir un styler etrange qui sorte de l'ordinaire",
+        description:"Un premier Projet effectué en école qui consistait à travailler avec des graphistes. <br>L' Objectif étant de créer deux sites vitrines pour un festival avec la contrainte d' avoir une seule page HTML<br>Les deux sites devais avoir un style étrange qui sorte de l'ordinaire",
         img1:"./images/paulWorkshop.png",
         titre1:"Site de Paul",
         paragraphe1:"Site realiser avec la colaboration de <a target=\"_blank\" href=\"https://readymag.com/u3810607477/4088804/\">Paul Figari</a> <br>Ici l'element principal est tourné vers l'oeuil en stopmotion et un theme un peu bleuatre avec des feuille.<br><a target=\"_blank\" href=\"https://math-pixel.github.io/workshop1/index.html?site=paul\">Lien du site</a>",
@@ -179,31 +179,31 @@ let contentVoirPlus = [
         paragraphe2:"Site realiser avec la colaboration de <a target=\"_blank\" href=\"https://readymag.com/u3995653634/4056188/\">Jeremy Berthey</a> <br>Ici l'element principal est tourné vers le robot en stopmotion et un theme un peu blanchatre avec un contraste elever avec le orange.<br><a target=\"_blank\" href=\"https://math-pixel.github.io/workshop1/index.html?site=jeremy\">Lien du site</a>"        
     },{
         name:"Jeux Video (WEB)",
-        description:"Jeux Video creé en HTML / CSS / JS, une direction artistique orienter illustration et un theme sur une taverne conviviale avec une ambiance sonore agreable<br>Le but : Retrouver la mascotte de la taverne qui ce cache derriere ces fut !",
+        description:"Jeux Vidéo créé en HTML / CSS / JS, une direction artistique orientée illustration et un thème sur une taverne conviviale avec une ambiance sonore agréable<br>Le but : Retrouver la mascotte de la taverne qui se cache derrière ces fûts !",
         img1:"./images/cache-cache.png",
         titre1:"Interface de jeu",
-        paragraphe1:"Le jeu possede plusieur niveaux et celon le niveau le nombre de click est limiter et la mascotte ce deplace aleatoirement toute les 2 secondes<br><a target=\"_blank\" href=\"https://math-pixel.github.io/taverne_game-web-/\">Lien du jeu</a>",
+        paragraphe1:"Le jeu possede plusieur niveaux et selon le niveau le nombre de clics est limitée et la mascotte se déplace aléatoirement toute les 2 secondes<br><a target=\"_blank\" href=\"https://math-pixel.github.io/taverne_game-web-/\">Lien du jeu</a>",
         img2:"",
         titre2:"",
         paragraphe2:""        
     },{
         name:"Volet Connecte",
-        description:"Suite au renovation de notre coproprieter les anciens volet manuel ont été remplacé par des volets automatique avec telecommande<br>Donc pour commencer une partit d' un gros project qui est de creer une maison connecté soit même j'ai commencer a connecté mon volet a un ordinateur pour le controler a distance",
+        description:"Suite au renovation de notre copropriété les anciens volet manuel ont été remplacé par des volets automatique avec télécommande<br>Donc pour commencer une partie d' un gros projet qui est de créer une maison connecté soit même j'ai commencer a connecté mon volet à un ordinateur pour le contrôler à distance",
         img1:"./images/volet.png",
         titre1:"Hardware",
-        paragraphe1:"Cette photo represente la partie Hardware ( electronique ) de mon volet<br>Il est constituer de trois elements <br>Un raspberry pi (RPI) qui sert dinterface entre le web ( le RPI est le serveur WEB ) et la telecommande,<br>Une plaque pcb qui sert de protection pour pouvoir piloter la telecommande sans cramer le RPI<br>Et la telecommande",
+        paragraphe1:"Cette photo représente la partie Hardware ( electronique ) de mon volet<br>Il est constitué de trois éléments <br>Un raspberry pi (RPI) qui sert d'interface entre le web ( le RPI est le serveur WEB ) et la télécommande,<br>Une plaque pcb qui sert de protection pour pouvoir piloter la télécommande sans cramer le RPI<br>Et la télécommande",
         img2:"./images/volet_ui.png",
         titre2:"Software",
-        paragraphe2:"Voici les 3 ecrans de mon application pour mon volet, la premiere sert de \" home \" la deuxieme simule ma commande numeriquement avec une touche en plus qui dit au volet de sarreter a une certaine position ( millieu ), et la 3 eme interface c'est les heures programmables de mon volet pour q'uil souvre et ce ferme a des heures precises"        
+        paragraphe2:"Voici les 3 écrans de mon application pour mon volet, la première sert de \" home \" la deuxième simule ma commande numériquement avec une touche en plus qui dit au volet de s'arrêter à une certaine position ( milieu ), et la 3 eme interface c'est les heures programmables de mon volet pour qu'il s'ouvre et se ferme à des heures précises"        
     },{
         name:"Filmotheque",
-        description:"Filmotheque collaborative creer lors d'un project final d'ecole. Une filmotheque est un site ou un endroit qui regroupe plusieur film et les tries pour pouvoir les retrouver et les harchivers",
+        description:"Filmothèque collaborative crée lors d'un projet final d'école. Une filmothèque est un site ou un endroit qui regroupe plusieur film et les tries pour pouvoir les retrouver et les archivés",
         img1:"./images/filmotheque.PNG",
         titre1:"Page d'accueil",
         paragraphe1:"<a target=\"_blank\" href=\"https://math-pixel.github.io/Filmotheque/\">Lien du site</a>",
         img2:"./images/recherche.PNG",
         titre2:"Fonction de recherche",
-        paragraphe2:"Parmis toutes les options du site une grosse fonction presente est la possibiliter de rechercher un film avec la bar de recherche ou avec un filtre près definie"        
+        paragraphe2:"Parmis toutes les options du site une grosse fonction présente est la possibilité de rechercher un film avec la bar de recherche ou avec un filtre pré définie"        
     }
 ]
 
